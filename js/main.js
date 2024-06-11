a006bddc5ee9e5b03fc6f7e658f9c2e7e72cfdff
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-11 18:49:41 +0200

    fix: fix click function

--- js/main.js
+++ js/main.js
@@ -1,22 +1,48 @@
 // Data una lista della spesa vuota;
 const shoppingList = [];
 
-// seleziono il valore dell'input dell'utente
-const itemInput = document.querySelector(".input_item").value ;
-
 // selezioni il bottone
 const button = document.querySelector(".button");
+
+// seleziono la lista in html
+const ul = document.querySelector(".list");
 
 // gestisco l'interazione al click
 button.addEventListener('click',
 
     function(){
+
+        // seleziono il valore dell'input dell'utente
+        const itemInput = document.querySelector(".input_item").value ; 
+
+        // aggiungo il balore dell'input all'array
         shoppingList.push(itemInput);
+
+        // stampo la lista in console
         console.log(shoppingList);
 
-        
+        // scorro gli elementi della lista (while)
+        // dichiaro la variabile indice fuori dal ciclo 
+        let li;
+        let i = 0;
+        while (i < shoppingList.length) {
+            
+            const shoppingItem = shoppingList[i];
+
+            i++
+
+            console.log(shoppingItem);
+
+            // creo l'elemento da aggiungere in html
+            li = `<li> ${shoppingItem} </li>`;
+
+        }
+
+        // aggiungo ogni elemento alla lista in html
+        ul.innerHTML += li;
     }
 
+    
 )
 
 
